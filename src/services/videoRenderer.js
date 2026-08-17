146ac758cff860b75ba243eb527023ac31184440
Waiphyo27 <kaungchun@gmail.com>
2026-08-17 16:41:15 +0630

    fix: add missing closing paren in subtitles filter

--- src/services/videoRenderer.js
+++ src/services/videoRenderer.js
@@ -223,6 +223,7 @@ async function renderVideo({
     const alignment = SUBTITLE_POSITIONS[subtitlePosition] || SUBTITLE_POSITIONS.bottom;
     filterLines.push(
       `[${currentLabel}]subtitles='${escapedSrtPath}':force_style='Fontname=Noto Sans\\,Alignment=${alignment}\\,MarginV=40'[vsub]`
+      );
     currentLabel = "vsub";
   }
 
